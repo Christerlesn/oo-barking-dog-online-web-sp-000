0cfb2c4c9b9d82711b18727e72b12959fec384fd
Christerle Nounoute <christerlesn@gmail.com>
2020-09-23 19:52:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,16 @@ def name
   @the_dog_name
 end
 
+def dog_sound
+  puts "woof!"
+end
+
 def bark=(dog_sound)
-  dog_sound = "Woof!"
   @dogs_bark = dog_sound
 end
 
 def bark
   @dogs_bark
 end
+
 end
